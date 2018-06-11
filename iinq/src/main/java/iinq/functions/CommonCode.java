240f8b9e6146de9cb2c3dc49deabf07e6c49e1f2
Kai <kaineub@gmail.com>
2018-06-10 19:03:04 -0700

    Add option for additional indents error checking code

--- iinq/src/main/java/iinq/functions/CommonCode.java
+++ iinq/src/main/java/iinq/functions/CommonCode.java
@@ -2,11 +2,25 @@ package iinq.functions;
 
 public final class CommonCode {
 	public static String
+	error_check(int extraIndents) {
+		StringBuilder errorCheck = new StringBuilder();
+		for (int i = 0; i < extraIndents; i++)
+			errorCheck.append("\t");
+		errorCheck.append("\tif (err_ok != error) {\n");
+		for (int i = 0; i < extraIndents; i++)
+			errorCheck.append("\t");
+		errorCheck.append("\t\tprintf(\"Error occurred. Error code: %i" + "\\" + "n" + "\", error);\n");
+		for (int i = 0; i < extraIndents; i++)
+			errorCheck.append("\t");
+		errorCheck.append("\t\treturn; \n");
+		for (int i = 0; i < extraIndents; i++)
+			errorCheck.append("\t");
+		errorCheck.append("\t}\n\n");
+		return errorCheck.toString();
+	}
+	public static String
 	error_check() {
-		return "\tif (err_ok != error) {\n" +
-				"\t\tprintf(\"Error occurred. Error code: %i" + "\\" + "n" + "\", error);\n" +
-				"\t\treturn; \n" +
-				"\t}\n\n";
+		return error_check(0);
 	}
 
 	public static String wrapInExecuteFunction(String codeFragment) {
